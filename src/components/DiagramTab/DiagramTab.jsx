6dedc945624b0bba5f8fb266444f7f12b222f3c1
IzabelaMalinowska <114566540+IzabelaMalinowska@users.noreply.github.com>
2023-09-25 21:52:27 +0200

    update

---
+++ src/components/DiagramTab/DiagramTab.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { transactionsSelectors } from '../../redux/finance/transaction-selectors';
+import { useSelector } from 'react-redux';
+import styles from './DiagramTab.module.css';
+export default function DiagramTab({ allArray }) { //TO DO: allArray
+  const categoriesFromState = useSelector(transactionsSelectors.getAllCategoriesFromTransactions);
+  return (
+    <div className={styles.container}>
+      <ul className={styles.expensesList}>
+        <li className={styles.expensesListColumnName}>Category</li>
+        <li className={styles.expensesListColumnName}>Sum</li>
+      </ul>
+      <ul className={styles.transactionList}>
+        {allArray?.length > 0 ? (
+          allArray.map(({ value, sum, color }) => {
+            return (
+              <li className={styles.transactionListElement}>
+                <div
+                  style={{
+                    backgroundColor: `${color}`,
+                    width: '24px',
+                    minHeight: '24px',
+                    borderRadius: '12px',
+                    marginRight: '15px',
+                  }}
+                ></div>
+                <div className={styles.category}>{value}</div>
+                <div className={styles.sum}>{sum}</div>
+              </li>
+            );
+          })
+        ) : (
+          <li className={styles.transactionListElement}>
+            <div className={styles.category}>Here is nothing :(</div>
+          </li>
+        )}
+      </ul>
+      <ul className={styles.totalList}>
+        <li className={styles.totalListElement}>
+          <div className={styles.totalListElementText}>Expenses:</div>
+          <div className={styles.totalListElementExpenses}>{categoriesFromState.consumption || 0}</div>
+        </li>
+        <li className={styles.totalListElement}>
+          <div className={styles.totalListElementText}>Income:</div>
+          <div className={styles.totalListElementIncome}>{categoriesFromState.income || 0}</div>
+        </li>
+      </ul>
+    </div>
+  );
+}
