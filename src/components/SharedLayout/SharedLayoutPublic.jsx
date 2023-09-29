2500cb4ff34bcb66eeee16786cb353495ac8ca02
DamTchorzewski <113952006+DamTchorzewski@users.noreply.github.com>
2023-09-29 02:54:55 +0200

    edit

--- src/components/SharedLayout/SharedLayoutPublic.jsx
+++ src/components/SharedLayout/SharedLayoutPublic.jsx
@@ -1,12 +1,11 @@
-
 import React, { Suspense } from 'react';
-import {Link, Outlet} from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import Loader from '../Loader/Loader';
 import useAuth from '../../utils/hooks/useAuth';
 
 
 const SharedLayoutPublic = () => {
-  // const { isAuthLoading } = useAuth();
+  const { isAuthLoading } = useAuth();
 
   return (
     <>
