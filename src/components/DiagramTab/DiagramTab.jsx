91d03cfd19c050100d549f56ca531aefdf5a5f8b
DamTchorzewski <113952006+DamTchorzewski@users.noreply.github.com>
2023-10-16 01:50:22 +0200

    Update DiagramTab.jsx

--- src/components/DiagramTab/DiagramTab.jsx
+++ src/components/DiagramTab/DiagramTab.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import styles from "./DiagramTab.module.css";
-import ChartComponent from "../chart/Chart";
+import ChartComponent from "../Chart/Chart.jsx";
 import StatisticsMenu from "../StatisticsMenu/StatisticsMenu";
 import { nanoid } from "nanoid";
 
